d62a8989ccc661946d75ef0cb9a56c5c8fa80188
Javier Méndez Gómez <novdomo@gmail.com>
2021-02-25 12:37:08 +0100

    Añadido unique not null al campo email de usuario. También al nombre de usuario propiamente dicho. Se elimina la tabla estado y el check pasa a la tabla estado_incidencia.

--- DB/db.sql
+++ DB/db.sql
@@ -5,37 +5,35 @@ DROP DATABASE IF EXISTS dbticketing;
 CREATE DATABASE dbticketing;
 USE dbticketing;
 
-
-CREATE TABLE incidencia(
-	id serial primary key,
-	prioridad character varying not null,
-	timestamp_creacion timestamp not null default now(),
-	descripcion text not null
+CREATE TABLE cargo (
+	cargo character varying primary key
 );
-CREATE TABLE estado (
-	estado character varying primary key,
-	CHECK (estado='Pendiente' or estado = 'En desarrollo' or estado = 'Resuelta')
+
+CREATE TABLE usuario (
+	id serial primary key,
+	email character varying unique not null,
+	usuario character varying unique not null,
+	clave character varying not null,
+	cargo character varying not null references cargo(cargo)
 );
 
 CREATE TABLE proyecto (
 	proyecto character varying primary key
 );
 
-CREATE TABLE cargo (
-	cargo character varying primary key
+CREATE TABLE incidencia(
+	id serial primary key,
+	email_usuario character varying references usuario(email),
+	proyecto character varying references proyecto(proyecto),
+	descripcion text not null,
+	timestamp_creacion timestamp not null default now(),
+	prioridad character varying not null
 );
 
 CREATE TABLE estado_incidencia (
 	id_incidencia bigint references incidencia(id),
 	estado character varying references estado(estado),
-	timestamp_estado timestamp not null default now()
+	CHECK (estado='Pendiente' or estado = 'En desarrollo' or estado = 'Resuelta'),
+	timestamp_estado timestamp default now(),
+	PRIMARY KEY (id_incidencia,estado,timestamp_estado)
 );
-
-CREATE TABLE usuario (
-	id serial primary key,
-	usuario character varying,
-    email character varying,
-    clave character varying,
-	cargo character varying references cargo(cargo)
-);
-
